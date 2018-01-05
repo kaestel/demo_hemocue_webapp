d8ebe09d4992e3d753a925aaed29a4ae457077eb
Martin Kæstel Nielsen <martin@think.dk>
2018-01-05 03:42:29 +0000

    bugfix

--- src/templates/shell.header.php
+++ src/templates/shell.header.php
@@ -1,5 +1,5 @@
-<? include_once($_SERVER['FRAMEWORK_PATH']."/include/segment.php") ?>
-<? include_once($_SERVER['FRAMEWORK_PATH']."/include/functions.inc.php") ?>
+<? include_once($_SERVER['LOCAL_PATH']."/includes/segment.php") ?>
+<? //include_once($_SERVER['FRAMEWORK_PATH']."/include/functions.inc.php") ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +12,11 @@
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
 	<? if($_SESSION["dev"]) { ?>
-		<link type="text/css" rel="stylesheet" media="all" href="/css/lib/seg_tablet_include.css" />
-		<script type="text/javascript" src="/js/lib/seg_tablet_include.js"></script>
+		<link type="text/css" rel="stylesheet" media="all" href="/css/lib/seg_<?= $_SESSION["segment"] ?>_include.css" />
+		<script type="text/javascript" src="/js/lib/seg_<?= $_SESSION["segment"] ?>_include.js"></script>
 	<? } else { ?>
-		<link type="text/css" rel="stylesheet" media="all" href="/css/seg_tablet.css" />
-		<script type="text/javascript" src="/js/seg_tablet.js"></script>
+		<link type="text/css" rel="stylesheet" media="all" href="/css/seg_<?= $_SESSION["segment"] ?>.css" />
+		<script type="text/javascript" src="/js/seg_<?= $_SESSION["segment"] ?>.js"></script>
 	<? } ?>
 </head>
 
